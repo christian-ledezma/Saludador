65b0da36d5d45cff8091dcf17a1c3d284b425341
christian-ledezma <chris.ledezma.s@gmail.com>
2025-02-15 21:46:32 -0400

    feat: Saludar de acuerdo a la hora.

--- src/saludador.js
+++ src/saludador.js
@@ -1,6 +1,17 @@
 function saludar(nombre){
+    let fechaActual = new Date();
+    let horaActual = fechaActual.getHours();
     if(nombre){
-        return `Hola querid@ ${nombre}!`;
+        if(horaActual >= 0 && horaActual < 12){
+            return `Buenos dias ${nombre}!`;
+        }
+        else if (horaActual >= 12 && horaActual < 18){
+            return `Buenas tardes ${nombre}!`;
+        }
+        else{
+            return `Buenas noches ${nombre}!`;
+        }
+
     }
     else{
         return `Ingresar nombre por favor!`
